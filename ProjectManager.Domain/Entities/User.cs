0cebf3e61aa2bebd2490f92f9ae331f548ea3b1e
Kruk24 <m.kruczynski04@gmail.com>
2025-05-31 17:04:50 +0200

    Added User and Project classes (#3)
    * Added basic User and Project classes
    
    * Added ProjectTask.cs, updated Project.cs

---
+++ ProjectManager.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+        public List<ProjectUser> ProjectUsers { get; set; } = new();
+        public List<ProjectUserRole> UserProjectsRoles { get; set; } = new();
+
+    }
+}
