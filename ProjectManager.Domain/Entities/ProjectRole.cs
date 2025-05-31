4a57251d07f8ae039f1ce052a9f492ae1c0cdfd7
Kruk24 <m.kruczynski04@gmail.com>
2025-05-31 17:04:50 +0200

    Added User and Project classes (#3)
    * Added basic User and Project classes
    
    * Added ProjectTask.cs, updated Project.cs

---
+++ ProjectManager.Domain/Entities/ProjectRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Entities
+{
+    public class ProjectRole
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; } = default!;
+        public Project Project { get; set; } = default!;
+        public string Name { get; set; } = default!;
+    }
+}
---
+++ ProjectManager.Domain/Entities/ProjectTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Entities
+{
+    public class ProjectTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string? AssignedUserId { get; set; }
+        public User? AssignedUser { get; set; }
+        public DateTime AssignmentTime { get; set; } = DateTime.Now;
+        public DateTime Deadline { get; set; } = default!;
+        public int ProjectId { get; set; } = default!;
+        public Project Project { get; set; } = default!;
+
+    }
+}
