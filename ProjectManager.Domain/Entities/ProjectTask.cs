2f733c5835a15f3e8fe1d8caa7e94014b0e9e995
Kruk24 <m.kruczynski04@gmail.com>
2025-06-01 22:33:33 +0200

    Added Task progress status, task comments, time logs and update other classes

--- ProjectManager.Domain/Entities/ProjectTask.cs
+++ ProjectManager.Domain/Entities/ProjectTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectManager.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
@@ -12,12 +13,14 @@ namespace ProjectManager.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
+        public TaskProgressStatus TaskProgressStatus { get; set; } = default!;
         public string? AssignedUserId { get; set; }
         public User? AssignedUser { get; set; }
         public DateTime AssignmentTime { get; set; } = DateTime.Now;
         public DateTime Deadline { get; set; } = default!;
         public int ProjectId { get; set; } = default!;
         public Project Project { get; set; } = default!;
+        public List<TaskComment> TaskComments { get; set; } = new List<TaskComment>();
 
     }
 }
