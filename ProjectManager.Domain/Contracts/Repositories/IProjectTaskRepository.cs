57a1970950606ddea1f0436629012ef2b2f52dae
Kruk24 <m.kruczynski04@gmail.com>
2025-06-02 22:12:14 +0200

    Added contract repositories' interfaces (#20)

---
+++ ProjectManager.Domain/Contracts/Repositories/IProjectTaskRepository.cs
@@ -0,0 +1,31 @@
+﻿using ProjectManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Contracts.Repositories
+{
+    public interface IProjectTaskRepository
+    {
+        Task Commit();
+        Task Create(ProjectTask projectTask);
+        Task DeleteTask(ProjectTask projectTask);
+        Task<IEnumerable<ProjectTask>> GetAllByEncodedName(string projectEncodedName);
+        Task<IEnumerable<ProjectTask>> GetUserProjectTasks(string projectEncodedName, string userId);
+        Task<IEnumerable<ProjectTask>> GetAllUserTasks(string userId);
+        Task<ProjectTask> GetById(int id);
+        Task Update(ProjectTask projectTask);
+        Task AddComment(TaskComment taskComment);
+        Task DeleteComment(TaskComment taskComment);
+        Task<IEnumerable<TaskComment>> GetTasksComments(int taskId);
+        Task<TaskComment?> GetCommentById(int id);
+        Task<IEnumerable<TaskComment>> GetUserComments(IEnumerable<int?> projectTaskIds, string userId);
+        Task DeleteComments(IEnumerable<TaskComment> userComments);
+        Task LogTime(TimeLog timeLog);
+        Task<IEnumerable<TimeLog>> GetTasksTimeLogs(int taskId);
+        Task<TimeLog> GetTimeLogById(int timeLogId);
+        Task DeleteTimeLog(TimeLog timelog);
+    }
+}
---
+++ ProjectManager.Domain/Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Contracts.Repositories
+{
+    public interface IUserRepository
+    {
+        Task Commit();
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserByEmail(string email);
+    }
+}
