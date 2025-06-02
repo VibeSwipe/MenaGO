3a3919f8b71e39cfa74822ce2da5cfcae85c3e67
AntekZ <151571894+AntekZ@users.noreply.github.com>
2025-06-02 22:13:39 +0200

    added handleExceptions (#13)
    Co-authored-by: Antek Z <antek@z.de>

---
+++ ProjectManager.Application/Common/CommandResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Common
+{
+    public class CommandResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public static CommandResult Success(string Message = "Success", int statusCode = 200)
+            => new CommandResult { IsSuccess = true, Message = Message, StatusCode = statusCode };
+
+        public static CommandResult Failure(string message, int statusCode = 400)
+            => new CommandResult { IsSuccess = false, Message = message, StatusCode = statusCode };
+    }
+}
