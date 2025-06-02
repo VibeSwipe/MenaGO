7b788e9355e049e6818e4aef56f4635f0f510b2c
Daniel Danys <daniel.kosk@neosystem.pl>
2025-06-02 22:19:33 +0200

    Added directory Users

---
+++ ProjectManager.Application/Users/Commands/EditUserRoles/EditUserRolesCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProjectManager.Application.ApplicationUser;
+using ProjectManager.Application.Common.Exceptions;
+using ProjectManager.Domain.Contracts.Repositories;
+using ProjectManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Commands.EditUserRoles
+{
+    public class EditUserRolesCommandHandler : IRequestHandler<EditUserRolesCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public EditUserRolesCommandHandler(IUserRepository userRepository, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userRepository = userRepository;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<Unit> Handle(EditUserRolesCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByEmail(request.Email);
+
+            if (user == null)
+            {
+                throw new NotFoundException("User not found");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in request.Roles)
+            {
+                if (!await _roleManager.RoleExistsAsync(role))
+                {
+                    continue;
+                }
+
+                if (!userRoles.Contains(role))
+                {
+                    await _userManager.AddToRoleAsync(user, role);
+                }
+            }
+
+            foreach (var role in userRoles)
+            {
+                if (!request.Roles.Contains(role))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, role);
+                }
+            }
+
+            return Unit.Value;
+        }
+    }
+
+}
