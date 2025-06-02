100bd40e220d6df98b757119c434de5a0797009e
1Danys <152191612+1Danys@users.noreply.github.com>
2025-06-02 22:44:40 +0200

    Feat users (#24)
    * Added UserContext
    
    * Added directory Users
    
    ---------
    
    Co-authored-by: Daniel Danys <daniel.kosk@neosystem.pl>

---
+++ ProjectManager.Application/Users/Queries/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Queries.GetAllUsers
+{
+    public class GetAllUsersQuery : IRequest<IEnumerable<UserDto>>
+    {
+    }
+}
---
+++ ProjectManager.Application/Users/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
