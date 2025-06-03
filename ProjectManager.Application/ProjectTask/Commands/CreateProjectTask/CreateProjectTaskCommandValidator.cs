2f984587a46e69bb61faa883c0d3c2c768de7902
AntekZ <151571894+AntekZ@users.noreply.github.com>
2025-06-03 18:37:23 +0200

    Feat edit log add create commands (#30)
    * Added Delete Commands
    
    * Added Edit Log Add Create commands

---
+++ ProjectManager.Application/ProjectTask/Commands/CreateProjectTask/CreateProjectTaskCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.ProjectTask.Commands.CreateProjectTask
+{
+    public class CreateProjectTaskCommandValidator : AbstractValidator<CreateProjectTaskCommand>
+    {
+        public CreateProjectTaskCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().NotNull()
+                .WithMessage("Enter task name");
+            RuleFor(p => p.Description).NotEmpty().NotNull()
+                .WithMessage("Enter task description");
+            RuleFor(p => p.Deadline)
+                .NotEmpty().NotNull().WithMessage("Enter task deadline date")
+                .Must(DateIsLaterThanNow).WithMessage("Task deadline date cannot be earlier than today's date");
+
+        }
+
+        private bool DateIsLaterThanNow(DateTime deadLine)
+        {
+            return deadLine > DateTime.UtcNow;
+        }
+    }
+}
+
---
+++ ProjectManager.Application/ProjectTask/Commands/EditProjectTask/EditProjectTaskCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using ProjectManager.Application.ProjectTask.Commands.CreateProjectTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.ProjectTask.Commands.EditProjectTask
+{
+    public class EditProjectTaskCommandValidator : AbstractValidator<EditProjectTaskCommand>
+    {
+        public EditProjectTaskCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().NotNull()
+                    .WithMessage("Enter task name");
+            RuleFor(p => p.Description).NotEmpty().NotNull()
+                .WithMessage("Enter task description");
+            RuleFor(p => p.Deadline)
+                .NotEmpty().NotNull().WithMessage("Enter task deadline date")
+                .Must(DateIsLaterThanNow).WithMessage("Task deadline date cannot be earlier than today's date");
+
+        }
+
+        private bool DateIsLaterThanNow(DateTime deadLine)
+        {
+            return deadLine > DateTime.UtcNow;
+        }
+
+    }
+}
