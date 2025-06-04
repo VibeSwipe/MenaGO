3f8576494fff498178ce8891d12810bdbf24c7dc
AntekZ <151571894+AntekZ@users.noreply.github.com>
2025-06-04 18:27:19 +0200

    Added Dto

---
+++ ProjectManager.Application/ProjectTask/ProjectTaskDto.cs
@@ -0,0 +1,22 @@
+﻿using ProjectManager.Domain.Entities;
+using ProjectManager.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.ProjectTask
+{
+    public class ProjectTaskDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public TaskProgressStatus TaskProgressStatus { get; set; } = default!;
+        public string? AssignedUserEmail { get; set; }
+        public DateTime AssignmentTime { get; set; } = DateTime.Now;
+        public DateTime Deadline { get; set; } = default!;
+        public List<CommentDto> TaskComments { get; set; } = new List<CommentDto>();
+    }
+}
