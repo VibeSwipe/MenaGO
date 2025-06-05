d5ae6dc50fcbbf485bc944b7d788454dd128bb40
Jakub Kucharek <131975376+Kuchaaar@users.noreply.github.com>
2025-06-05 21:27:44 +0200

    add ProjectDto and Project commands and queries (#36)

---
+++ ProjectManager.Application/Project/Commands/EditProject/EditProjectCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using ProjectManager.Application.ApplicationUser;
+using ProjectManager.Domain.Contracts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project.Commands.EditProject
+{
+    public class EditProjectCommandHandler : IRequestHandler<EditProjectCommand>
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IUserContext _userContext;
+
+        public EditProjectCommandHandler(IProjectRepository projectRepository, IUserContext userContext)
+        {
+            _projectRepository = projectRepository;
+            _userContext = userContext;
+        }
+        public async Task<Unit> Handle(EditProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetByEncodedName(request.EncodedName!);
+
+            var user = _userContext.GetCurrentUser();
+            var isEditable = user != null && (project.CreatedById == user.Id || user.IsInRole("Admin"));
+
+            if (!isEditable)
+            {
+                return Unit.Value;
+            }
+
+            project.Description = request.Description;
+            project.FinishDate = request.FinishDate;
+
+            await _projectRepository.Commit();
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ ProjectManager.Application/Project/ProjectDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project
+{
+    public class ProjectDto
+    {
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public string? EncodedName { get; set; }
+        public List<Domain.Entities.ProjectTask> ProjectTasks { get; set; } = new();
+        public List<Domain.Entities.ProjectUser> ProjectContributors { get; set; } = new();
+        public DateTime FinishDate { get; set; } = default!;
+        public bool IsEditable { get; set; }
+    }
+}
